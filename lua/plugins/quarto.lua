dbe845aad50b7c13618975b776df22a20d4f5927
juanQNav <juan.qn363@gmail.com>
2025-07-23 00:50:27 -0600

    aux commit

---
+++ lua/plugins/quarto.lua
@@ -0,0 +1,241 @@
+return {
+  { -- requires plugins in lua/plugins/treesitter.lua and lua/plugins/lsp.lua
+    -- for complete functionality (language features)
+    "quarto-dev/quarto-nvim",
+    dev = false,
+    opts = {
+      lspFeatures = {
+        enabled = true,
+        chunks = "curly",
+      },
+      codeRunner = {
+        enabled = true,
+        default_method = "slime",
+      },
+    },
+    dependencies = {
+      -- for language features in code cells
+      -- configured in lua/plugins/lsp.lua
+      "jmbuhr/otter.nvim",
+    },
+    config = function(_, opts)
+      require("quarto").setup(opts)
+
+      -- Keymaps Quarto
+      local quarto = require("quarto")
+      vim.keymap.set("n", "<leader>Qp", quarto.quartoPreview, {
+        desc = "Quarto Preview",
+        silent = true,
+        noremap = true,
+      })
+      vim.keymap.set("n", "<leader>Qr", quarto.quartoClosePreview, {
+        desc = "Quarto Close Preview",
+        silent = true,
+        noremap = true,
+      })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "quarto", "markdown", "qmd" },
+        callback = function()
+          vim.opt_local.conceallevel = 0
+        end,
+      })
+      vim.keymap.set("n", "<leader>cp", function()
+        -- Abrir terminal en split horizontal
+        --vim.cmd("split")
+        --endfunction Activar venv y luego python3
+        --vim.cmd("terminal bash -c 'source .venv/bin/activate && python3'")
+        --let Cambiar a modo insert para interactuar inmediatamente
+        --vim.cmd("startinsert")
+        vim.cmd("vsplit term://python")
+      end, {
+        desc = "Open Python terminal with .venv",
+        silent = true,
+        noremap = true,
+      })
+
+      -- insert python code block (.qmd)
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "quarto", "qmd" },
+        callback = function()
+          vim.keymap.set("i", "<S-A-l>", function()
+            local lines = {
+              "```{python}",
+              "",
+              "```",
+            }
+            local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+            vim.api.nvim_buf_set_lines(0, row, row, false, lines)
+            vim.api.nvim_win_set_cursor(0, { row + 2, 0 })
+          end, {
+            desc = "Insert Python code block",
+            buffer = true,
+          })
+
+          -- normal mode
+          vim.keymap.set("n", "<S-A-l>", function()
+            local lines = {
+              "```{python}",
+              "",
+              "```",
+            }
+            local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+            vim.api.nvim_buf_set_lines(0, row, row, false, lines)
+            vim.api.nvim_win_set_cursor(0, { row + 2, 0 })
+            vim.cmd("startinsert")
+          end, {
+            desc = "Insert Python code block",
+            buffer = true,
+          })
+        end,
+      })
+    end,
+  },
+  { -- directly open ipynb files as quarto docuements
+    -- and convert back behind the scenes
+    "GCBallesteros/jupytext.nvim",
+    opts = {
+      custom_language_formatting = {
+        python = {
+          extension = "qmd",
+          style = "quarto",
+          force_ft = "quarto",
+        },
+        r = {
+          extension = "qmd",
+          style = "quarto",
+          force_ft = "quarto",
+        },
+      },
+    },
+  },
+
+  { -- send code from python/r/qmd documets to a terminal or REPL
+    -- like ipython, R, bash
+    "jpalardy/vim-slime",
+    dev = false,
+    init = function()
+      vim.b["quarto_is_python_chunk"] = false
+      Quarto_is_in_python_chunk = function()
+        require("otter.tools.functions").is_otter_language_context("python")
+      end
+
+      vim.cmd([[
+      let g:slime_dispatch_ipython_pause = 100
+      function SlimeOverride_EscapeText_quarto(text)
+      call v:lua.Quarto_is_in_python_chunk()
+      if exists('g:slime_python_ipython') && len(split(a:text,"\n")) > 1 && b:quarto_is_python_chunk && !(exists('b:quarto_is_r_mode') && b:quarto_is_r_mode)
+      return ["%cpaste -q\n", g:slime_dispatch_ipython_pause, a:text, "--", "\n"]
+      else
+      if exists('b:quarto_is_r_mode') && b:quarto_is_r_mode && b:quarto_is_python_chunk
+      return [a:text, "\n"]
+      else
+      return [a:text]
+      end
+      end
+      endfunction
+      ]])
+
+      vim.g.slime_target = "neovim"
+      vim.g.slime_no_mappings = true
+      vim.g.slime_python_ipython = 1
+    end,
+    config = function()
+      vim.g.slime_input_pid = false
+      vim.g.slime_suggest_default = true
+      vim.g.slime_menu_config = false
+      vim.g.slime_neovim_ignore_unlisted = true
+
+      local function mark_terminal()
+        local job_id = vim.b.terminal_job_id
+        vim.print("job_id: " .. job_id)
+      end
+
+      local function set_terminal()
+        vim.fn.call("slime#config", {})
+      end
+      vim.keymap.set("n", "<leader>Qm", mark_terminal, { desc = "[m]ark terminal" })
+      vim.keymap.set("n", "<leader>Qs", set_terminal, { desc = "[s]et terminal" })
+    end,
+  },
+
+  { -- paste an image from the clipboard or drag-and-drop
+    "HakonHarnes/img-clip.nvim",
+    event = "BufEnter",
+    ft = { "markdown", "quarto", "latex" },
+    opts = {
+      default = {
+        dir_path = "img",
+      },
+      filetypes = {
+        markdown = {
+          url_encode_path = true,
+          template = "![$CURSOR]($FILE_PATH)",
+          drag_and_drop = {
+            download_images = false,
+          },
+        },
+        quarto = {
+          url_encode_path = true,
+          template = "![$CURSOR]($FILE_PATH)",
+          drag_and_drop = {
+            download_images = false,
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("img-clip").setup(opts)
+      vim.keymap.set("n", "<leader>Qi", ":PasteImage<cr>", { desc = "insert [i]mage from clipboard" })
+    end,
+  },
+
+  { -- preview equations
+    "jbyuki/nabla.nvim",
+    keys = {
+      { "<leader>Qm", ':lua require"nabla".toggle_virt()<cr>', desc = "toggle [m]ath equations" },
+    },
+  },
+
+  {
+    "benlubas/molten-nvim",
+    dev = false,
+    enabled = true,
+    version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+    build = ":UpdateRemotePlugins",
+    init = function()
+      vim.g.molten_image_provider = "image.nvim"
+      -- vim.g.molten_output_win_max_height = 20
+      vim.g.molten_auto_open_output = true
+      vim.g.molten_auto_open_html_in_browser = true
+      vim.g.molten_tick_rate = 200
+    end,
+    config = function()
+      local init = function()
+        local quarto_cfg = require("quarto.config").config
+        quarto_cfg.codeRunner.default_method = "molten"
+        vim.cmd([[MoltenInit]])
+      end
+      local deinit = function()
+        local quarto_cfg = require("quarto.config").config
+        quarto_cfg.codeRunner.default_method = "slime"
+        vim.cmd([[MoltenDeinit]])
+      end
+      vim.keymap.set("n", "<localleader>Qmi", init, { silent = true, desc = "Initialize molten" })
+      vim.keymap.set("n", "<localleader>Qmd", deinit, { silent = true, desc = "Stop molten" })
+      vim.keymap.set("n", "<localleader>Qmp", ":MoltenImagePopup<CR>", { silent = true, desc = "molten image popup" })
+      vim.keymap.set(
+        "n",
+        "<localleader>Qmb",
+        ":MoltenOpenInBrowser<CR>",
+        { silent = true, desc = "molten open in browser" }
+      )
+      vim.keymap.set("n", "<localleader>Qmh", ":MoltenHideOutput<CR>", { silent = true, desc = "hide output" })
+      vim.keymap.set(
+        "n",
+        "<localleader>Qms",
+        ":noautocmd MoltenEnterOutput<CR>",
+        { silent = true, desc = "show/enter output" }
+      )
+    end,
+  },
+}
