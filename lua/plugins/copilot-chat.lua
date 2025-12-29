aeb5ea17f4cc0bc9d334f2ea566a0f4a1bf484a4
juanQNav <juan.qn363@gmail.com>
2025-12-28 22:56:03 -0600

    feat: add conventional argparse promt

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -29,6 +29,7 @@ local prompts = {
               and idiomatic, and follow standard English naming conventions (camelCase, snake_case, etc. as appropriate for the language)."]],
   ConventionalCommit = [["Generate a Conventional Commit message for the following code changes using the format
                     'type(scope): description'. Choose the appropriate type (feat, fix, docs, refactor, etc.) and write a clear, concise description."]],
+  ConventionalArgparser = "Please provide provide better conventional argparse for the following code, include long and short option (1 character).",
 }
 
 return {
