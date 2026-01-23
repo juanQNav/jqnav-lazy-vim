896580d0f313cb23e838e6ec7ac2b1ece1ebe0e8
juanQNav <juan.qn363@gmail.com>
2026-01-22 20:19:58 -0600

    feat: add keymap for toggle transparency

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -36,7 +36,12 @@ return {
         operators = {},
       },
       color_overrides = {},
-      custom_highlights = {},
+      custom_highlights = function(colors)
+        return {
+          -- Make floating windows transparent
+          NormalFloat = { bg = "NONE" },
+        }
+      end,
       default_integrations = true,
       integrations = {
         cmp = true,
@@ -52,5 +57,18 @@ return {
     })
 
     vim.cmd.colorscheme("catppuccin")
+
+    -- function for toggle transparency
+    local transparen_bg = true
+    _G.toggle_transparency = function()
+      transparen_bg = not transparen_bg
+      require("catppuccin").setup({
+        transparent_background = transparen_bg,
+      })
+      custom_highlights = {}, vim.cmd.colorscheme("catppuccin")
+    end
+
+    -- keymap to toggle transparency
+    vim.keymap.set("n", "<leader>ut", "<cmd>lua toggle_transparency()<CR>", { desc = "Toggle Transparency" })
   end,
 }
