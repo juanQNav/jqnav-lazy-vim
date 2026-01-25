d91fd07c6fdd3fdb758926aa998025ec1dba024e
juanQNav <juan.qn363@gmail.com>
2026-01-24 22:27:59 -0600

    feat: improve render checkbox

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -13,6 +13,20 @@ return {
         converter = "latex2text",
         highlight = "RenderMarkdownMath", -- String, no table
       },
+      checkbox = {
+        unchecked = {
+          icon = "☐", -- Customize icon
+          -- highlight = "RenderMarkdownChecked",
+          -- scope_highlight = "RenderMarkdownChecked",
+        },
+        checked = {
+          icon = "󰄲", -- Customize icon
+          -- highlight = "RenderMarkdownChecked",
+          -- scope_highlight = "RenderMarkdownChecked",
+        },
+        right_pad = 6,
+        left_pad = 0,
+      },
     })
   end,
 }
