9a6842102c36845d813fd04dd20e14d0b08e4403
juanQNav <juan.qn363@gmail.com>
2026-01-26 01:07:31 -0600

    feat: add todo commments

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,44 @@
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    signs = true, -- show icons in the signs column
+    sign_priority = 8, -- sign priority
+    keywords = {
+      TODO = { icon = " ", color = "info", alt = { "TODO" } },
+      DONE = { icon = " ", color = "#6b7280", alt = { "DONE" } },
+    },
+    highlight = {
+      multiline = true, -- enable multine todo comments
+      multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
+      multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
+      before = "", -- "fg" or "bg" or empty
+      -- #FIX: markdown render font with withe color, then dont have contrast with bg highlight
+      keyword = "wide_bg", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+      after = "", -- "fg" or "bg" or empty
+      pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
+      comments_only = false, -- uses treesitter to match keywords in comments only
+      max_line_len = 400, -- ignore lines longer than this
+      exclude = {}, -- list of file types to exclude highlighting
+    },
+    gui_style = {
+      fg = "NONE", -- The gui style to use for the fg highlight group.
+      bg = "BOLD", -- The gui style to use for the bg highlight group.
+    },
+    search = {
+      command = "rg",
+      args = {
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+      },
+      -- regex that will be used to match keywords.
+      -- don't replace the (KEYWORDS) placeholder
+      pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+      -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+    },
+    merge_keywords = true,
+  },
+}
