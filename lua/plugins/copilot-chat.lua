feef053fb4f0ac5da7922ebb77f286622919a3f3
juanQNav <juan.qn363@gmail.com>
2026-02-27 02:05:36 -0600

    feat: set up a file picked for copilot chat

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -162,6 +162,24 @@ Your goal is to make every answer useful, clear, and aligned with my style: prof
         ":CopilotChatHelp<cr>",
         desc = "Show help",
       },
+      -- set up a file picker dor copilot chat
+      {
+        "<leader>af",
+        function()
+          require("telescope.builtin").find_files({
+            attach_mappings = function(_, map)
+              map("i", "<CR>", function(prompt_bufnr)
+                local selection = require("telescope.actions.state").get_selected_entry()
+                require("telescope.actions").close(prompt_bufnr)
+                vim.api.nvim_put({ selection[1] }, "c", true, true)
+              end)
+              return true
+            end,
+          })
+        end,
+        desc = " File picker for Copilot Chat",
+        mode = { "n", "v" },
+      },
     },
     config = function(_, opts)
       local chat = require("CopilotChat")
