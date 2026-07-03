1729e82a85170cb89c4376ddd0391f2c1325de3c
juanQNav <juan.qn363@gmail.com>
2026-07-03 10:28:45 -0600

    feat(mistake): scope activation to markdown and text files

--- lua/plugins/mistake.lua
+++ lua/plugins/mistake.lua
@@ -1,13 +1,22 @@
 return {
   "ck-zhang/mistake.nvim",
   config = function()
-    local plugin = require("mistake")
-    vim.defer_fn(function()
-      plugin.setup()
-    end, 500)
+    local mistake = require("mistake")
 
-    vim.keymap.set("n", "<leader>ma", plugin.add_entry, { desc = "[M]istake [A]dd entry" })
-    vim.keymap.set("n", "<leader>me", plugin.edit_entries, { desc = "[M]istake [E]dit entries" })
-    vim.keymap.set("n", "<leader>mc", plugin.add_entry_under_cursor, { desc = "[M]istake add [C]urrent word" })
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "markdown", "text" },
+      callback = function(ev)
+        mistake.setup()
+        local b = ev.buf
+        vim.keymap.set("n", "<leader>ma", mistake.add_entry, { buffer = b, desc = "[M]istake [A]dd entry" })
+        vim.keymap.set("n", "<leader>me", mistake.edit_entries, { buffer = b, desc = "[M]istake [E]dit entries" })
+        vim.keymap.set(
+          "n",
+          "<leader>mc",
+          mistake.add_entry_under_cursor,
+          { buffer = b, desc = "[M]istake add [C]urrent word" }
+        )
+      end,
+    })
   end,
 }
