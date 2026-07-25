ae4aed56230a7be4374847e3d0406ef8bf8c66ec
juanQNav <juan.qn363@gmail.com>
2026-07-24 23:15:55 -0600

    feat(euporie): add euporie.nvim plugin for Jupyter notebook support

---
+++ lua/plugins/euporie.lua
@@ -0,0 +1,8 @@
+return {
+  "bxrne/euporie.nvim",
+  opts = {
+    path = ".", -- Directory to open the notebook session in
+    graphics_dpi = 300, -- DPI for graphics rendering (default: 300)
+    graphics_height = 40, -- Maximum graphics height in pixels (default: 40)
+  },
+}
