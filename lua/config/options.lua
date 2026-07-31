1674c1bd4d4959d54256579cd4fe612750b4dd6b
juanQNav <juan.qn363@gmail.com>
2026-07-30 20:22:08 -0600

    feat(cmd-spell): add auto download sapanish spell dictionary if missing

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,3 +8,17 @@ vim.diagnostic.config({
 
 -- Spell check: English + Spanish
 vim.opt.spelllang = { "en", "es" }
+
+-- Auto-download Spanish spell dictionary if missing
+vim.api.nvim_create_autocmd("VimEnter", {
+  once = true,
+  callback = function()
+    local spell_dir = vim.fn.stdpath("data") .. "/site/spell"
+    local spl_file = spell_dir .. "/es.utf-8.spl"
+    if vim.fn.filereadable(spl_file) == 0 then
+      vim.fn.mkdir(spell_dir, "p")
+      vim.cmd("set spelllang=es")
+      vim.cmd("set spell")
+    end
+  end,
+})
