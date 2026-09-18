38769b84e5f0c160d15c0ec42c66d78126e00d08
juanQNav <juan.qn363@gmail.com>
2026-09-17 22:25:26 -0600

    feat(nvim): add custom keymaps for vim-visual-multi plugin

--- lua/plugins/multi-line.lua
+++ lua/plugins/multi-line.lua
@@ -1,4 +1,11 @@
 return {
   "mg979/vim-visual-multi",
   event = { "BufReadPost", "BufNewFile" },
+  init = function()
+    vim.g.VM_maps = {
+      ["Add Cursor Down"] = "<C-M-J>",
+      ["Add Cursor Up"] = "<C-M-K>",
+      -- ["Select All"] = "<C-a>",
+    }
+  end,
 }
